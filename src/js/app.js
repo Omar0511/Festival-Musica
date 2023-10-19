8222f316ab5a6bad6a65a837401fd82993e73845
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2023-10-18 22:59:11 -0600

    Añadiendo un boton para cerarrar la IMG

--- src/js/app.js
+++ src/js/app.js
@@ -30,33 +30,39 @@ function crearGaleria()
     }
 }
 
-
 function mostrarImagen(id) {
     const imagen = document.createElement('picture');
+
     imagen.innerHTML = `
         <source srcset="build/img/grande/${id}.avif" type="image/avif">
         <source srcset="build/img/grande/${id}.webp" type="image/webp">
-        <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen_galeria">
+        <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria">
     `;
 
+    // Crear overlay con la imagen
     const overlay = document.createElement('DIV');
     overlay.appendChild(imagen);
     overlay.classList.add('overlay');
+    // Ayuda a cerrar la IMG donde quiera que demos click
+    overlay.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
 
-
+    // Boton para cerrar el Modal
     const cerrarModal = document.createElement('P');
     cerrarModal.textContent = 'X';
     cerrarModal.classList.add('btn-cerrar');
     cerrarModal.onclick = function() {
         const body = document.querySelector('body');
         body.classList.remove('fijar-body');
-
-        overlay.remove(cerrarModal);
+        overlay.remove();
     }
     overlay.appendChild(cerrarModal);
 
+    // Añadirlo al HTML
     const body = document.querySelector('body');
     body.appendChild(overlay);
     body.classList.add('fijar-body');
-
 }

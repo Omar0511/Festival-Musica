17510c8c638faf731c5a1afb41f6ce0fe9445100
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2023-10-20 00:14:17 -0600

    Añadiendo un efecto Scrool smoth

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,24 @@ document.addEventListener('DOMContentLoaded', function()
 function iniciarApp()
 {
     crearGaleria();
+    scrollNav();
+}
+
+function scrollNav()
+{
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', function(e) 
+        {
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.values;
+            
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({behavior: "smooth"});
+        });
+    });
 }
 
 function crearGaleria()

010a4ece24485a77f91e3a3523d33ac23d191b06
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2023-10-20 00:36:24 -0600

    Detectar Scroll con Javascript

--- build/js/app.js
+++ build/js/app.js
@@ -7,24 +7,45 @@ function iniciarApp()
 {
     crearGaleria();
     scrollNav();
+    navegacionFija();
 }
 
-function scrollNav()
+function navegacionFija()
+{
+    const barra = document.querySelector('.header');
+
+    const sobreFestival = document.querySelector('.sobre-festival');
+
+    window.addEventListener('scroll', function()
+    {
+        if (sobreFestival.getBoundingClientRect().bottom < 0)
+        {
+            barra.classList.add('fijo');
+        }
+        else
+        {
+            barra.classList.remove('fijo');
+        }
+    });
+}
+
+function scrollNav() 
 {
     const enlaces = document.querySelectorAll('.navegacion-principal a');
 
-    enlaces.forEach( enlace => {
+    enlaces.forEach( enlace => 
+    {
         enlace.addEventListener('click', function(e) 
         {
-            e.preventDefault();
+            e.preventDefault(); 
 
-            const seccionScroll = e.target.attributes.href.values;
-            
+            const seccionScroll = e.target.attributes.href.value;
             const seccion = document.querySelector(seccionScroll);
-            seccion.scrollIntoView({behavior: "smooth"});
+            seccion.scrollIntoView( { behavior: "smooth" } );
         });
     });
 }
+
 
 function crearGaleria()
 {
